f10e06fbb7a825fd8e321c8de5fa251d6161bcf2
Aaron Bill Domingo <domingoaaronbill@gmail.com>
2020-02-16 19:43:49 +0800

    get not supported error

--- app/bill.php
+++ app/bill.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class bill extends Model
 {
     //
-    public function printed_records()
+    /* public function printed_records()
     {
         return $this->hasMany("App\printed_record", "bill_id", 'id');
-    }
+    } */
 }
--- app/printed_record.php
+++ app/printed_record.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class printed_record extends Model
 {
     //
-    public function student_bill()
+    /* public function student_bill()
     {
         return $this->belongsTo('App\bill', 'id', 'bill_id');
-    }
+    } */
 }
