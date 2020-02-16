b9c74f3efa7c97a7f23b0297f5a741411b63cfac
Aaron Bill Domingo <domingoaaronbill@gmail.com>
2020-02-16 20:09:51 +0800

    test

--- public/js/vue/functions.js
+++ public/js/vue/functions.js
@@ -16,7 +16,7 @@ function create_student_bill(data)
 {
     $.ajax({
         data: data,
-        type: "POS",
+        type: "POST",
         url: "/create/bill",
         beforeSend: function (request) {
             return request.setRequestHeader('X-CSRF-Token', $("meta[name='csrf-token']").attr('content'));
