299f20eedf394e716794db3e68e5703602ca5c50
Aaron Bill Domingo <domingoaaronbill@gmail.com>
2020-02-18 15:09:00 +0800

    update one migration file

--- database/migrations/2020_02_12_071242_add_columns_to_bills.php
+++ database/migrations/2020_02_12_071242_add_columns_to_bills.php
@@ -15,6 +15,16 @@ class AddColumnsToBills extends Migration
     {
         Schema::table('bills', function (Blueprint $table) {
             //
+            $table->float('unpaid_previous_account', 8, 2);
+            $table->float('total_fees_for_sy', 8, 2);
+            $table->float('tuition_fee', 8, 2);
+            $table->float('registration_fee', 8, 2);
+            $table->float('others_books', 8, 2);
+            $table->float('tuition_fee_as_of_value', 8, 2);
+            $table->float('registration_fee_as_of_value', 8, 2);
+            $table->float('books_as_of_value', 8, 2);
+            $table->float('outstanding_balance', 8, 2);
+            $table->float('amount_payable_for_this_month_value', 8, 2);
         });
     }
 
@@ -27,16 +37,7 @@ class AddColumnsToBills extends Migration
     {
         Schema::table('bills', function (Blueprint $table) {
             //
-            $table->float('unpaid_previous_account', 8, 2);
-            $table->float('total_fees_for_sy', 8, 2);
-            $table->float('tuition_fee', 8, 2);
-            $table->float('registration_fee', 8, 2);
-            $table->float('others_books', 8, 2);
-            $table->float('tuition_fee_as_of_value', 8, 2);
-            $table->float('registration_fee_as_of_value', 8, 2);
-            $table->float('books_as_of_value', 8, 2);
-            $table->float('outstanding_balance', 8, 2);
-            $table->float('amount_payable_for_this_month_value', 8, 2);
+
         });
     }
 }
